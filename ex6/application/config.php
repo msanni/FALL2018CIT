36fc886010947fc4aac3e93fd8ab10d8a689b0b5
Machioud Sanni <msanni@iu.edu>
2018-11-11 12:44:24 -0500

    ex6

---
+++ ex6/application/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:23721/ex6/');
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'msanni');
+define('DB_PASS', '1stAccount');
+define('DB_NAME', 'msanni_db');
