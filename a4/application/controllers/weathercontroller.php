a1ff2de8bbb92eb4be9a5a42e16fb98effa06df7
Machioud Sanni <msanni@iu.edu>
2018-11-30 14:16:37 -0500

    final1 updates

--- a4/application/controllers/weathercontroller.php
+++ a4/application/controllers/weathercontroller.php
@@ -8,8 +8,7 @@ class WeatherController extends Controller{
 
 	}
         public function getresults() {
-        $xml = simplexml_load_file("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=119fae7a2f784643a00225741181511
-&q=".$_POST['zip']."&format=xml&num_of_days=2");
+        $xml = simplexml_load_file("http://api.worldweatheronline.com/premium/v1/weather.ashx?key=119fae7a2f784643a00225741181511&q=".$_POST['zip']."&format=xml&num_of_days=2");
         $this->set('result', true);
         $this->set('weather', $xml);
         }
--- final1/views/home/index.php
+++ final1/views/home/index.php
@@ -10,7 +10,7 @@ include('views/elements/header.php');?>
 
 
 	<div style="float: left; width: 60%;">
-		<h2>Final Project</h2>
+		<h2>Sanni's Final Project</h2>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra, sapien sed lacinia pharetra, nibh orci congue felis, sit amet bibendum dolor tellus eget mi. Nullam sit amet ligula at quam dignissim tempus at at neque. Cras id massa orci. Nulla congue velit eget pulvinar molestie. Vivamus ullamcorper rutrum dolor sit amet congue. In fermentum enim laoreet pharetra sagittis. Ut vestibulum eget ligula et sollicitudin. Vestibulum sed arcu dapibus, porta turpis sit amet, venenatis felis. Cras et facilisis turpis. Integer quis interdum nulla.
 
 		</p>
